076333c07be7e82e972b9d6a0d9864c6ed4f959b
ADMIN <huydo164@gmail.com>
2021-01-26 08:52:21 +0700

    update

---
+++ bai16/vue.js
@@ -0,0 +1,15 @@
+var test = new Vue({
+    data: {
+        foo: 1
+    },
+    computed: {
+        bar: function() {
+            console.log(bar);
+        }
+    },
+    methods: {
+        baz: function() {
+            console.log(baz);
+        }
+    }
+})
